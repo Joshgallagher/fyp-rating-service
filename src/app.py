60fcc15f1076616379d216bfad812f27a0e52e35
Josh <joshgallagher24@gmail.com>
2020-04-27 00:10:23 +0100

    Implemented MongoDB ORM and updated endpoints

--- src/app.py
+++ src/app.py
@@ -1,11 +1,26 @@
-from flask import Flask, jsonify
+from flask import Flask, request, Response
+from core.database import init_connection
+from rating.model.rating import Rating
 
 app = Flask(__name__)
+app.config['MONGODB_SETTINGS'] = {
+    'host': 'mongodb://root:example@rating-service-db:27017/rating?authSource=admin'
+}
+init_connection(app)
+
+
+@app.route('/<article>', methods=['GET'])
+def get(article):
+    rating = Rating.objects(articleId=article).count()
+    return {'rating': int(rating)}, 200
 
 
 @app.route('/', methods=['POST'])
 def create():
-    return jsonify({'created': True})
+    body = request.get_json()
+    rating = Rating(**body).save()
+    id = rating.id
+    return {'id': str(id)}, 201
 
 
 if __name__ == '__main__':
