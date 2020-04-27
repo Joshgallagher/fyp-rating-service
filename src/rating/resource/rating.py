2b088610903bb981f3d8ea9f9cc55875a7ce95d3
Josh <joshgallagher24@gmail.com>
2020-04-27 18:34:20 +0100

    Created get_subject middleware to extract the subject from the JWT and pass it to the Resource

--- src/rating/resource/rating.py
+++ src/rating/resource/rating.py
@@ -1,17 +1,20 @@
-from flask import request
+import sys
+
+from flask import g, request
 from flask_restful import Resource
 from rating.model.rating import Rating
-from middleware.user_id_header import user_id_header
+from middleware.get_subject import get_subject
 
 
 class RatingsResource(Resource):
-    method_decorators = {'post': [user_id_header]}
+    method_decorators = {'post': [get_subject]}
 
     def get(self, id):
         rating = Rating.objects(articleId=id).count()
         return {'rating': int(rating)}, 200
 
     def post(self, id):
+        print('POST:' + g.current_user_id, file=sys.stderr)
         userId = request.get_json()['userId']
         rating = Rating(userId=userId, articleId=id).save()
         id = rating.id
