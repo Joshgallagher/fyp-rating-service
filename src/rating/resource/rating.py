4a2b518500e00923e71c44e7a60354fe153eeb96
Josh <joshgallagher24@gmail.com>
2020-04-27 20:10:26 +0100

    Created authorise middleware

--- src/rating/resource/rating.py
+++ src/rating/resource/rating.py
@@ -1,12 +1,12 @@
-import sys
-
-from flask import g, request
+from flask import g
 from flask_restful import Resource
 from rating.model.rating import Rating
 from middleware.get_subject import get_subject
+# from middleware.authorise import authorise
 
 
 class RatingsResource(Resource):
+    # method_decorators = {'post': [authorise, get_subject]}
     method_decorators = {'post': [get_subject]}
 
     def get(self, id):
@@ -14,8 +14,7 @@ class RatingsResource(Resource):
         return {'rating': int(rating)}, 200
 
     def post(self, id):
-        print('POST:' + g.current_user_id, file=sys.stderr)
-        userId = request.get_json()['userId']
+        userId = g.current_user_id
         rating = Rating(userId=userId, articleId=id).save()
         id = rating.id
         return {'id': str(id)}, 201
